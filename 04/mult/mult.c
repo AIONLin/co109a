5972f2ae7552ad735f752b933e4d8cbde7c73c7c
AIONLin <a13181496529765@gmail.com>
2021-01-16 11:51:16 +0800

    add other way

--- 04/mult/mult.c
+++ 04/mult/mult.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+int main() {
+    int R0 = 5;
+    int R1 = 3;
+    int R2 = 0;
+loop:    
+    // while (R0 > 0) { //
+    if (R0<=0) goto endLoop; 
+      R2 = R2 + R1;             //用加法實現乘法
+      R0 = R0 - 1;
+      printf("R0=%d R1=%d R2=%d\n", R0, R1, R2);
+    // }
+    goto loop;
+endLoop:    
+    printf("R2=%d\n", R2);
+}
+
+
 /*
 R0=1
 while (1){
     R0=R0+1;
-}*/
+}
 
 int main(){
     int R0 = 1;
@@ -29,10 +46,6 @@ D;JLE  //if R0>0
 @3
 D=A
 @1
-M=D
-
-
-
-if
+M=D/*
 
 
