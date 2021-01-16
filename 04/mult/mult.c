c34fde3160bed2f30c5812aeea075cfc1dec5d65
AIONLin <a13181496529765@gmail.com>
2021-01-16 12:03:14 +0800

    Error fix

--- 04/mult/mult.c
+++ 04/mult/mult.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int R0 = 5;
+    int R0 = 5;               
     int R1 = 3;
     int R2 = 0;
 loop:    
     // while (R0 > 0) { //
     if (R0<=0) goto endLoop; 
       R2 = R2 + R1;             //用加法實現乘法
-      R0 = R0 - 1;
+      R0 = R0 - 1;              // 加過後-1
       printf("R0=%d R1=%d R2=%d\n", R0, R1, R2);
     // }
     goto loop;
